4cb9dc1f435cbecc14bb054a430eb27d8b75b7bb
Nathan <nathan@Nathans-MacBook-Pro.local>
2021-03-02 14:54:47 +0000

    created swipe functionality along with button swipe function

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image} from "react-native";
+import { Card, Button } from "react-native-elements";
+import Deck from "./src/Deck";
+import { data } from './data';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  renderCard(item) {
+    return ( 
+      <View style={styles.card}>     
+        <Text style={styles.textStyle}>{item.desc}</Text>
+        <Image style={styles.image} source={{ uri: item.uri }} />
+        <Text style={styles.cardText}>{item.text}</Text>
+      </View>
+
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title="All Done!">
+        <Text style={{ marginBottom: 10 }}>There is no more content here!</Text>
+        <Button backgroundColor="#03A9FA" title="Get more!" />
+      </Card>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logoImage} source={require('./assets/mi3.png')}/>
+        <Deck
+          data={data}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 500,
+    borderRadius: 4,
+    marginBottom: 10,
+  },
+  logoImage: {
+    width: 250,
+    height: 50,
+    marginTop: 50,
+    marginLeft:'auto',
+    marginRight: 'auto',
+  },
+  card: {
+    borderRadius: 20,
+    backgroundColor: '#FFF',
+    padding: 10
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#CCC",
   },
+  cardText: {
+    marginLeft:'auto',
+    marginRight: 'auto',
+    fontSize: 20
+  },
+  textStyle: {
+    marginLeft:'auto',
+    marginRight: 'auto',
+    marginBottom: 20,
+    fontSize: 20
+  }
 });
---
+++ data.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    id: 1,
+    text: "Tom",
+    uri: "https://i.imgur.com/DQZbPPc.jpg",
+    desc: "Stunt Double"
+  },
+  {
+    id: 2,
+    text: "Biff",
+    uri: "https://i.imgur.com/YZvLHw8.jpg",
+    desc: "Actor"
+  },
+  {
+    id: 3,
+    text: "Slick James",
+    uri: "https://i.imgur.com/in5VTbq.jpg",
+    desc: "Producer"
+  },
+  {
+    id: 4,
+    text: "Rob",
+    uri: "https://i.imgur.com/gcVSy6P.jpg",
+    desc: "Director"
+  },
+  {
+    id: 5,
+    text: "Sol",
+    uri: "https://i.imgur.com/274ojqI.jpg",
+    desc: "Executive Producer"
+  },
+  {
+    id: 6,
+    text: "Terry",
+    uri: "https://i.imgur.com/OzrM0WF.jpg",
+    desc: "Writer"
+  },
+  {
+    id: 7,
+    text: "Mark",
+    uri: "https://i.imgur.com/ntoCLIU.jpg",
+    desc: "Extra"
+  },
+  
+];
